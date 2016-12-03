07c8f57171fed1c8adba0b342a093649a424bd97
Rafal Fusik <rafalfusik@Rafals-MacBook-Air.local>
2016-12-03 12:14:01 +0100

    move browser and get driver

---
+++ utils/browser.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+
+
+def get_driver(conf):
+
+    if conf.get('browser') == 'Firefox':
+        binary = FirefoxBinary(conf.get('firefox_binary'))
+        driver = webdriver.Firefox(firefox_binary=binary)
+    elif conf.get('browser') == 'Chrome':
+        driver = webdriver.Chrome(conf.get('chrome_driver'))
+    elif conf.get('browser') == 'Ie':
+        driver = webdriver.Ie(executable_path=conf.get('ie_driver'))
+    elif conf.get('browser') == 'Safari':
+        driver = webdriver.Safari()
+    return driver
